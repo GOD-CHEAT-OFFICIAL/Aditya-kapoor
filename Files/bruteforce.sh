4d8968735091fc2057249a5380697ae18c5b0b64
GOD-CHEAT-OFFICIAL <khushikumari09229@gmail.com>
2024-08-16 19:48:31 +0530

    Create bruteforce.sh

---
+++ Files/bruteforce.sh
@@ -0,0 +1,5 @@
+exit &>/dev/null          <
+/system/bin/sh: cd: /storage/emulated/0/Android/data/org.telegram.messenger/files/Telegram/Telegram Files: Permission denied
+sh: /storage/emulated/0/Android/data/org.telegram.messenger/files/Telegram/Telegram Files/bruteforce.sh: Permission denied
+
+[Process completed (error 127) - press Enter]
